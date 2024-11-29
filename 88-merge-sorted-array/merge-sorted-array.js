b460a915391ca449ab2cf4088a7257d32489edab
M Abdullah <150430204+connect-abdullah@users.noreply.github.com>
2024-11-29 16:56:11 +0500

    Time: 0 ms (100.00%) | Memory: 49.1 MB (58.18%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+const merge = function(nums1, m, nums2, n) {
+    // Pointers for nums1 and nums2
+    let p1 = m - 1; // Last valid element in nums1
+    let p2 = n - 1; // Last element in nums2
+    let p = m + n - 1; // Last position in nums1
+
+    // Merge in reverse order
+    while (p1 >= 0 && p2 >= 0) {
+        if (nums1[p1] > nums2[p2]) {
+            nums1[p] = nums1[p1];
+            p1--;
+        } else {
+            nums1[p] = nums2[p2];
+            p2--;
+        }
+        p--;
+    }
+
+    // If there are remaining elements in nums2, copy them
+    while (p2 >= 0) {
+        nums1[p] = nums2[p2];
+        p2--;
+        p--;
+    }
+};
+
+// Example usage:
+let nums1 = [1, 2, 3, 0, 0, 0];
+let m = 3;
+let nums2 = [2, 5, 6];
+let n = 3;
+
+merge(nums1, m, nums2, n);
+console.log(nums1); // Output: [1, 2, 2, 3, 5, 6]
