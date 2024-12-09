c8f1a54dadd44699e7f642eb353d8f9df73a9a7d
M Abdullah <150430204+connect-abdullah@users.noreply.github.com>
2024-12-09 20:53:49 +0500

    Time: 11 ms (79.48%) | Memory: 63.7 MB (16.93%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const containsDuplicate = (nums) => {
+    const seen = new Set()
+    for (const num of nums) {
+        if (seen.has(num)) return true;
+        seen.add(num)
+    }
+    return false
+
+}
